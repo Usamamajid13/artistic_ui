6357ee46bfc0ce708a1be095393a9294258916b0
Usama Majid <83748784+Usamamajid13@users.noreply.github.com>
2022-03-02 22:45:22 +0500

    Added text field widget 2 March 2022 / 10:45 PM

--- lib/src/artistic_ui.dart
+++ lib/src/artistic_ui.dart
@@ -246,3 +246,169 @@ class _CustomButtonDoubleTextState extends State<CustomButtonDoubleText> {
     );
   }
 }
+
+class BorderTextField extends StatefulWidget {
+  ///this is the controller which controls what happens to the text you type in text field
+  final TextEditingController inputController;
+
+  ///this is the title that comes on top outside the text field
+  var title;
+
+  ///this is the title font size
+  var titleFontSize;
+
+  ///this is the space between the text field and the title
+  var spaceBetweenTitle;
+
+  ///this is the height of the text field
+  var textFieldHeight;
+
+  ///this is the input hint given inside the text field
+  String inputHint;
+
+  ///this is the border color which is given to the text field
+  var borderColor;
+
+  ///this is the hint text color
+  var inputHintColor;
+
+  ///this is the title font weight (bold or italic, etc)
+  var titleFontWeight;
+
+  ///this is the icon color if the icon is given
+  var prefixIconColor;
+
+  ///this is the padding inside the text field
+  var contextHorizontalPadding;
+
+  ///this is the vertical content padding inside the text field
+  var contextVerticalPadding;
+
+  ///this is the input font size
+  var inputFontSize;
+
+  ///this is the color of the text written in the text field
+  var inputColor;
+
+  ///this is the width of the text field
+  var textFieldWidth;
+
+  ///this is the horizontal margin for the text field from the screen
+  var horizontalMargin;
+
+  ///this is the prefix icon
+  var prefixIcon;
+
+  ///this is the circular radius for the container. If you want to you can give it to the field
+  var fieldBorderRadius;
+
+  ///this is the color of the title text outside the text field
+  var titleColor;
+
+  ///this is the input type that specify the keyboard type when user click on the text field
+  TextInputType inputType;
+
+  ///constructor
+  BorderTextField({
+    Key? key,
+    required this.inputController,
+    this.title,
+    this.textFieldWidth,
+    this.contextHorizontalPadding,
+    this.contextVerticalPadding,
+    required this.inputHint,
+    this.horizontalMargin,
+    required this.borderColor,
+    this.titleFontSize,
+    required this.inputType,
+    this.inputColor,
+    this.inputHintColor,
+    this.spaceBetweenTitle,
+    this.fieldBorderRadius,
+    this.inputFontSize,
+    this.prefixIcon,
+    this.prefixIconColor,
+    this.textFieldHeight,
+    this.titleColor,
+    this.titleFontWeight,
+  }) : super(key: key);
+
+  @override
+  State<BorderTextField> createState() => _BorderTextFieldState();
+}
+
+class _BorderTextFieldState extends State<BorderTextField> {
+  @override
+  Widget build(BuildContext context) {
+    ///this is the main widget
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.title?.toString() ?? "",
+          style: TextStyle(
+            fontSize: widget.titleFontSize?.toDouble() ?? 18,
+            fontWeight: widget.titleFontWeight ?? FontWeight.bold,
+            color: widget.titleColor ?? Colors.black,
+          ),
+        ),
+        SizedBox(
+          height: widget.spaceBetweenTitle?.toDouble() ?? 0,
+        ),
+
+        ///this is the text field widget
+        Container(
+          width: widget.textFieldWidth?.toDouble() ??
+              MediaQuery.of(context).size.width,
+          height: widget.textFieldHeight?.toDouble() ?? 50,
+          margin: EdgeInsets.symmetric(
+              horizontal: widget.horizontalMargin?.toDouble() ?? 0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(
+                widget.fieldBorderRadius?.toDouble() ?? 0),
+            border: Border.all(color: widget.borderColor ?? Colors.red),
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: widget.prefixIcon == null ? 0 : 20,
+              ),
+              widget.prefixIcon == null
+                  ? Container()
+                  : Icon(
+                      widget.prefixIcon,
+                      size: 20,
+                      color: widget.prefixIconColor ??
+                          widget.borderColor ??
+                          Colors.black,
+                    ),
+              Expanded(
+                child: TextField(
+                  controller: widget.inputController,
+                  keyboardType: widget.inputType,
+                  style: TextStyle(
+                      fontSize: widget.inputFontSize?.toDouble() ?? 14,
+                      color: widget.inputColor ?? Colors.black),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: widget.inputHint,
+                    hintStyle: TextStyle(
+                        color: widget.inputHintColor ??
+                            Colors.grey.withOpacity(.75)),
+                    contentPadding: EdgeInsets.symmetric(
+                        vertical:
+                            widget.contextVerticalPadding?.toDouble() ?? 0.0,
+                        horizontal:
+                            widget.contextHorizontalPadding?.toDouble() ??
+                                20.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
